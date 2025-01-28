874fdd092c0380d756339f7e646aff02b1eebc6a
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-01-27 21:32:42 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is an end-to-end solution designed to analyze the sentiment of YouTube comments. It leverages Natural Language Processing (NLP) techniques to classify comments as positive, negative, or neutral. The plugin extracts comments via the YouTube API, processes the data, and provides actionable insights through an intuitive interface. Built for developers and content creators, this tool helps in understanding audience sentiment and improving engagement strategies.',
+    author='fahimX',
+    license='',
+)
